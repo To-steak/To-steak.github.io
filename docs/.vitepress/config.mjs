ba5537b73444456c539ba190430e80cee39603a9
To_steak <70811061+To-steak@users.noreply.github.com>
2026-08-28 16:57:30 +0900

    docs: config 설정
    노션 북마크 시 description이 나오기 때문

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: "To_steak's Devlog",
-  description: "Unity Client",
+  description: "Unity",
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' }
